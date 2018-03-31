cc4cee2adfc3f03be7318272da71c6ce1319b5b2
kusairi <riskadelina96@gmail.com>
2018-04-01 06:03:49 +0800

    mengisi nilai atibut

--- src/praktek02/kulkasaksi.java
+++ src/praktek02/kulkasaksi.java
@@ -1,6 +1,11 @@
 package praktek02;
 public class kulkasaksi {
+    
     public static void main(String[] args) {
         kulkas t1 = new kulkas ();
+        t1.merek = "samsung";
+        t1.harga = 6000000;
+        t1.jumlah = 1;  
+        
     }
 }

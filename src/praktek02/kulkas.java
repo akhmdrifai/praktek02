ba629f2fc0d1685610ae7db98099f7ec6125117d
kusairi <riskadelina96@gmail.com>
2018-04-01 05:59:46 +0800

    Revert "Membuat attribut dan behavior"
    This reverts commit d725cf16623c9d70b5c15a009656d6ed6db8afa2.

--- src/praktek02/kulkas.java
+++ src/praktek02/kulkas.java
@@ -1,14 +1,4 @@
 package praktek02;
 public class kulkas {
-       String merek;
-       double harga;
-       String jumlah;
-       
-       void pendingin(){
-           System.out.println("=================");
-           System.out.println("merek : "+merek);
-           System.out.println("harga : "+harga);
-           System.out.println("jumlah : "+jumlah);
-           System.out.println("=================");
-       }
+    
 }
